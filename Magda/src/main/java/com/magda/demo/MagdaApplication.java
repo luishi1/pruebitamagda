e7f8ea6122d32cf72085a55490404719073b86f5
Luishi1 <luis.oviedo.et26@gmail.com>
2024-12-09 00:34:06 -0300

    subo prueba para magda

---
+++ Magda/src/main/java/com/magda/demo/MagdaApplication.java
@@ -0,0 +1,13 @@
+package com.magda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagdaApplication.class, args);
+	}
+
+}
---
+++ Magda/src/main/java/com/magda/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.magda.demo.repository;
+
+import com.magda.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
